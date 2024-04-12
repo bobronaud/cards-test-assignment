33b2b1aee41e2d6b838514423ed02666fb772459
bobronaud <bobronaud@gmail.com>
2024-04-12 16:40:45 +0300

    load logos from assets

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,6 +6,10 @@ import { openModal } from '../slices/modalSlice';
 import { setCurrentCompanyId } from '../slices/cardsSlice';
 import EyeImage from './EyeImage';
 import TrashImage from './TrashImage';
+import logo1 from '../assets/images/logo1.png';
+import logo2 from '../assets/images/logo2.png';
+import logo3 from '../assets/images/logo3.png';
+import bm from '../assets/images/bm.png';
 
 type Props = {
   data: CardType;
@@ -14,6 +18,20 @@ type Props = {
 const Card = forwardRef(({ data }: Props, ref) => {
   const dispatch = useAppDispatch();
   const { company, customerMarkParameters, mobileAppDashboard } = data;
+  const logoType = mobileAppDashboard.logo
+    .replace('http://bonusmoney.info/image/mail/', '')
+    .slice(0, -4);
+
+  interface Logo {
+    [key: string]: string;
+  }
+
+  const logo: Logo = {
+    logo1,
+    logo2,
+    logo3,
+    bm,
+  };
 
   const handleClick = (type: 'show' | 'remove' | 'info') => {
     dispatch(setCurrentCompanyId(company.companyId));
@@ -22,57 +40,51 @@ const Card = forwardRef(({ data }: Props, ref) => {
 
   return (
     <div
-      className="card"
+      className='card'
       // @ts-ignore
       ref={ref}
-      style={{ backgroundColor: mobileAppDashboard.cardBackgroundColor }}
-    >
-      <div className="card-header">
+      style={{ backgroundColor: mobileAppDashboard.cardBackgroundColor }}>
+      <div className='card-header'>
         <p style={{ color: mobileAppDashboard.highlightTextColor }}>
           {mobileAppDashboard.companyName}
         </p>
-        <img className="company-logo" src={mobileAppDashboard.logo} />
+        <img className='company-logo' src={logo[logoType]} />
       </div>
       <hr />
-      <div className="card-body">
-        <div className="card-body-points" style={{ color: mobileAppDashboard.textColor }}>
+      <div className='card-body'>
+        <div className='card-body-points' style={{ color: mobileAppDashboard.textColor }}>
           <span
-            className="points-value"
-            style={{ color: mobileAppDashboard.highlightTextColor }}
-          >
+            className='points-value'
+            style={{ color: mobileAppDashboard.highlightTextColor }}>
             {customerMarkParameters.loyaltyLevel.requiredSum}
           </span>
           баллов
         </div>
-        <div className="flex-row">
+        <div className='flex-row'>
           <div
-            className="card-body-cashback"
-            style={{ color: mobileAppDashboard.textColor }}
-          >
+            className='card-body-cashback'
+            style={{ color: mobileAppDashboard.textColor }}>
             Кешбэк
             <span
-              className="cashback-value"
-              style={{ color: mobileAppDashboard.highlightTextColor }}
-            >
+              className='cashback-value'
+              style={{ color: mobileAppDashboard.highlightTextColor }}>
               {`${customerMarkParameters.loyaltyLevel.markToCash} %`}
             </span>
           </div>
           <div
-            className="card-body-level"
-            style={{ color: mobileAppDashboard.textColor }}
-          >
+            className='card-body-level'
+            style={{ color: mobileAppDashboard.textColor }}>
             Уровень
             <span
-              className="level-value"
-              style={{ color: mobileAppDashboard.highlightTextColor }}
-            >
+              className='level-value'
+              style={{ color: mobileAppDashboard.highlightTextColor }}>
               {customerMarkParameters.loyaltyLevel.name}
             </span>
           </div>
         </div>
       </div>
       <hr />
-      <div className="card-footer">
+      <div className='card-footer'>
         <div onClick={() => handleClick('show')}>
           <EyeImage color={mobileAppDashboard.mainColor} />
         </div>
@@ -80,13 +92,12 @@ const Card = forwardRef(({ data }: Props, ref) => {
           <TrashImage color={mobileAppDashboard.accentColor} />
         </div>
         <button
-          className="button"
+          className='button'
           onClick={() => handleClick('info')}
           style={{
             color: mobileAppDashboard.mainColor,
             backgroundColor: mobileAppDashboard.backgroundColor,
-          }}
-        >
+          }}>
           Подробнее
         </button>
       </div>
